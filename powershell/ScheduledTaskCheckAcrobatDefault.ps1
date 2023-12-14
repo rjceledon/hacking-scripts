005b8a1de21e52304d653bcbf9fb5052c7ea0ab4
rjceledon <85322110+rjceledon@users.noreply.github.com>
2023-12-14 13:36:02 -0500

    Update ScheduledTaskCheckAcrobatDefault.ps1

--- powershell/ScheduledTaskCheckAcrobatDefault.ps1
+++ powershell/ScheduledTaskCheckAcrobatDefault.ps1
@@ -1,4 +1,4 @@
-$powershell_code = {
+$Powershell_Code = {
     If (!(Test-Path 'C:\Temp' -PathType Container)) {
         New-Item -ItemType Directory -Force -Path 'C:\Temp'
     }
@@ -6,30 +6,28 @@ $powershell_code = {
     Invoke-WebRequest -Uri 'https://kolbi.cz/SetUserFTA.zip' -OutFile 'C:\Temp\SetUserFTA.zip'
     Expand-Archive -Path 'C:\Temp\SetUserFTA.zip' -DestinationPath 'C:\Temp' -Force
     Remove-Item  'C:\Temp\SetUserFTA.zip' -Force
-    Write-Host '[ ] Getting PDF association'
-    $Result = cmd.exe /c 'C:\Temp\SetUserFTA\SetUserFTA.exe get | findstr .pdf'
+    Write-Host '[ ] Getting MAILTO association'
+    $Result = cmd.exe /c 'C:\Temp\SetUserFTA\SetUserFTA.exe get | findstr mailto'
     Write-Host "[+] Current association is: $Result"
-    If (!($Result -ilike '*acrobat*')) {
-        Write-Host '[ ] Checking if Acrobat is installed'
-        If (Get-Package | findstr /I Acrobat) {
-            Write-Host "[+] Acrobat is installed, chaging association" -ForegroundColor Green
-            Start-Process -Wait -WindowStyle Hidden -FilePath 'C:\Temp\SetUserFTA\SetUserFTA.exe' -ArgumentList '.pdf Acrobat.Document.DC'
+    If (!($Result -ilike '*outlook*')) {
+        Write-Host '[ ] Checking if Outlook is installed'
+        If (Get-ItemProperty HKLM:\SOFTWARE\Classes\Outlook.Application -ErrorAction SilentlyContinue) {
+            Write-Host "[+] Outlook is installed, chaging association" -ForegroundColor Green
+            Start-Process -Wait -WindowStyle Hidden -FilePath 'C:\Temp\SetUserFTA\SetUserFTA.exe' -ArgumentList 'mailto Outlook.URL.mailto.15'
         }
         Else {
-            Write-Host '[!] Acrobat is NOT installed, exiting' -ForegroundColor Red
+            Write-Host '[!] Outlook is NOT installed, exiting' -ForegroundColor Red
         }
     }
-    $Result = cmd.exe /c 'C:\Temp\SetUserFTA\SetUserFTA.exe get | findstr .pdf'
+    $Result = cmd.exe /c 'C:\Temp\SetUserFTA\SetUserFTA.exe get | findstr mailto'
     Write-Host "`n[=] Final association is: $Result"
 }
-
-$encodedCommand = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($powershell_code.ToString()))
-
-$taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -EncodedCommand $encodedCommand"
-$taskTrigger = New-ScheduledTaskTrigger -AtLogOn
-$taskName = 'CheckAcrobatAssociation'
-$taskPrincipal = New-ScheduledTaskPrincipal -GroupId "Users" -RunLevel Limited
-$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-$task = New-ScheduledTask -Action $taskAction -Principal $taskPrincipal -Settings $taskSettings -Trigger $taskTrigger -Description 'Check and apply Acrobat as PDF launcher' 
-Unregister-ScheduledTask -TaskName $taskName -Confirm:$false -ErrorAction SilentlyContinue
-Register-ScheduledTask -TaskName $taskName -InputObject $task -TaskPath '\'
+$EncodedCommand = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Powershell_Code.ToString()))
+$TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -EncodedCommand $EncodedCommand"
+$TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
+$TaskName = 'CheckOutlookAssociation'
+$TaskPrincipal = New-ScheduledTaskPrincipal -GroupId "Users" -RunLevel Limited
+$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+$Task = New-ScheduledTask -Action $TaskAction -Principal $TaskPrincipal -Settings $TaskSettings -Trigger $TaskTrigger -Description 'Check and apply Outlook as MAILTO launcher' 
+Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction SilentlyContinue
+Register-ScheduledTask -TaskName $TaskName -InputObject $Task -TaskPath '\'
